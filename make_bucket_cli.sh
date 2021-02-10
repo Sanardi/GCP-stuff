8b1c36a20d89736b8cbdb4ae39ad0a7efdce246c
Sanardi <rubypromise@gmx.com>
2021-02-10 09:39:13 +0000

    Update make_bucket_cli.sh

--- make_bucket_cli.sh
+++ make_bucket_cli.sh
@@ -6,10 +6,9 @@ gcloud auth list
 
 gcloud config list project
 
-
-
-gcloud config set compute/zone us-east1-b
-gcloud config set compute/region us-east1
+#for this you need permission to create computer resources:
+#gcloud config set compute/zone us-east1-b
+#gcloud config set compute/region us-east1
 
 export PROJECT=sanardi
 
@@ -24,7 +23,6 @@ gsutil -m cp -R gs://spls/gsp290/dataflow-python-examples .
 #gsutil mb -c regional -l us-central1 gs://$PROJECT
 
 wget http://www.ssa.gov/OACT/babynames/names.zip
-
 
 
 gsutil cp gs://spls/gsp290/data_files/usa_names.csv gs://$PROJECT/data_files/
